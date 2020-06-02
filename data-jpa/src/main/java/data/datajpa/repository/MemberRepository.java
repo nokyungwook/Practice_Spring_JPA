f1bb1c81e4ebb0a517b03fc8695603bc714ee249
Player <shruddnr12@gmail.com>
2020-06-02 17:41:31 +0900

    수강한 강의 실습 ------------------------------------- 프로젝트 환경설정 프로젝트 생성 라이브러리 살펴보기 H2 데이터베이스 설치 스프링 데이터 JPA와 DB 설정, 동작확인 예제 도메인 모델 예제 도메인 모델과 동작확인 --------------------------------------

---
+++ data-jpa/src/main/java/data/datajpa/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package data.datajpa.repository;
+
+
+import data.datajpa.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+}
